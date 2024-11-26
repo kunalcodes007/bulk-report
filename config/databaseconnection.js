c720bd7d7ba6a3210920250b1a15496372fa388d
kunalcodes007 <kunallingwal2@gmail.com>
2024-11-26 11:51:06 +0530

    feat:fixed

---
+++ config/databaseconnection.js
@@ -0,0 +1,23 @@
+const database = require('../config/database');
+const ErrorHandler = require("../Utils/ErrorHandler");
+
+const db = async (query, value) => {
+  let connection;
+  try {
+    connection = await database();
+
+    const [rows] = await connection.execute(query, value);
+    return rows;
+
+  } catch (error) {
+    console.error('Error executing query:', error);
+    throw new ErrorHandler(error.message, 500);
+
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
+  }
+};
+
+module.exports = db;
